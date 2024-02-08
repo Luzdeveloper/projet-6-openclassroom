d19acbe4bf9398655ad0462d0396eb5a5bb46468
LuzDev <140513070+Luzdeveloper@users.noreply.github.com>
2024-02-08 10:53:11 +0100

    fetch modifier

--- FrontEnd/Javascript/script.js
+++ FrontEnd/Javascript/script.js
@@ -1,7 +1,7 @@
 fetch("http://localhost:5678/api/works")
 .then(response => response.json())
-.then((json) => console.log(json))
+.then((works) => console.log(works))
 
 fetch("http://localhost:5678/api/categories")
 .then(response => response.json())
-.then((json) => console.log(json))
+.then((categories) => console.log(categories))
