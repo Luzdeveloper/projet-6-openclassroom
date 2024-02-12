d5ea7da8ab28449e4e7988910355c96c88bf10b8
LuzDev <140513070+Luzdeveloper@users.noreply.github.com>
2024-02-12 12:43:33 +0100

    affichage de la gallerie et des sous titre des images

--- FrontEnd/Javascript/script.js
+++ FrontEnd/Javascript/script.js
@@ -1,7 +1,35 @@
-fetch("http://localhost:5678/api/works")
-.then(response => response.json())
-.then((works) => console.log(works))
+const API_URL="http://localhost:5678/api"
 
-fetch("http://localhost:5678/api/categories")
-.then(response => response.json())
-.then((categories) => console.log(categories))
+//affichage de la gallerie et des sous titres
+
+
+function getApiWorks(){
+    fetch(`${API_URL}/works`)
+        .then(response => response.json())
+        .then((works) => {
+            console.log(works)
+            displayWorks(works)
+        })
+        .catch(error => console.error('erreur affichage gallerie:', error))
+}
+
+function getApiCategories() {
+    fetch (`${API_URL}/categories`)
+        .then(response => response.json())
+        .then((categories) => console.log(categories))
+        .catch(error => console.error('erreur affichage categories', error))
+}
+
+function displayWorks(works){
+    const container = document.querySelector('.gallery')
+    container.innerHTML = works.map(work => `
+        <figure>
+            <img src="${work.imageUrl}">
+            <figcaption>${work.title}</figcaption>
+            </figure>`
+        ).join('')
+}
+
+getApiWorks()
+getApiCategories()
+
