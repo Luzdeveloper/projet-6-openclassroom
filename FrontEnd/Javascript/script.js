4cbd4a1ccacf4d4339ab833518a00cd50d7f78b2
LuzDev <140513070+Luzdeveloper@users.noreply.github.com>
2024-02-14 16:51:14 +0100

    affichage bouton + mise en place module

--- FrontEnd/Javascript/script.js
+++ FrontEnd/Javascript/script.js
@@ -1,35 +1,8 @@
-const API_URL="http://localhost:5678/api"
-
-//affichage de la gallerie et des sous titres
+import { getApiWorks, getApiCategories } from "./fetch.js";
 
 
-function getApiWorks(){
-    fetch(`${API_URL}/works`)
-        .then(response => response.json())
-        .then((works) => {
-            console.log(works)
-            displayWorks(works)
-        })
-        .catch(error => console.error('erreur affichage gallerie:', error))
-}
 
-function getApiCategories() {
-    fetch (`${API_URL}/categories`)
-        .then(response => response.json())
-        .then((categories) => console.log(categories))
-        .catch(error => console.error('erreur affichage categories', error))
-}
+getApiWorks();
+getApiCategories();
 
-function displayWorks(works){
-    const container = document.querySelector('.gallery')
-    container.innerHTML = works.map(work => `
-        <figure>
-            <img src="${work.imageUrl}">
-            <figcaption>${work.title}</figcaption>
-            </figure>`
-        ).join('')
-}
-
-getApiWorks()
-getApiCategories()
 
