b16b5faaec5e5a04566e1c8c920eac6484534222
LuzDev <140513070+Luzdeveloper@users.noreply.github.com>
2024-02-15 12:34:07 +0100

    filtre fonctionnel

--- FrontEnd/Javascript/script.js
+++ FrontEnd/Javascript/script.js
@@ -1,8 +1,61 @@
-import { getApiWorks, getApiCategories } from "./fetch.js";
+const API_URL="http://localhost:5678/api"
+
+function getApiWorks() {
+    return fetch(`${API_URL}/works`)
+      .then(response => response.json())
+      .then((data) => {
+          console.table(data);
+          displayWorks(data);
+          return data;
+      })
+      .catch(error => console.error('Error displaying gallery:', error));
+  }
+  
+  function getApiCategories() {
+    return fetch(`${API_URL}/categories`)
+      .then(response => response.json())
+      .then((categories) => {
+        console.table(categories);
+        displayCategories(categories);
+      })
+      .catch(error => console.error('Error displaying categories:', error));
+  }
+  
+  getApiWorks();
+  getApiCategories();
+  
+  function displayWorks(works) {
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = works.map(work => `
+      <figure>
+        <img src="${work.imageUrl}">
+        <figcaption>${work.title}</figcaption>
+      </figure>
+    `).join('');
+  }
+  
+  function displayCategories(categories) {
+    const filters = document.querySelector('.filters');
+    console.log(categories);
+    categories.forEach(category => {
+      const button = document.createElement('button');
+      button.id = category.id;
+      button.innerText = category.name;
+      button.classList.add('filterButton');
+      filters.appendChild(button);
+  
+      button.addEventListener('click', function() {
+        getApiWorks().then(works => {
+          const filteredWorks = works.filter(work => work.categoryId === category.id);
+          console.table(filteredWorks);
+          displayWorks(filteredWorks);
+        });
+      });
+    });
+  }
 
 
 
-getApiWorks();
-getApiCategories();
+
 
 
